274b1d3a0a16a010b3a6521c68fe30a896ae05e5
Chandler Squires <csquires@mit.edu>
2020-12-10 16:14:11 -0500

    writing cone program to files in python

--- py_utils/random_program.py
+++ py_utils/random_program.py
@@ -14,21 +14,42 @@ def random_cone_prog(m, n, cone_dict):
     x_star = np.random.randn(n)
     b = A @ x_star + s_star
     c = -A.T @ y_star
-    return A, b, c
+    return dict(A=A, b=b, c=c, x_star=x_star, y_star=y_star, s_star=s_star)
 
 
-def save_cone_program(folder, A, b, c):
+def save_cone_program(folder, program):
     os.makedirs(folder, exist_ok=True)
-    sparse.save_npz(f'{folder}/A.npz', A)
-    np.savetxt(f'{folder}/b.txt', b)
-    np.savetxt(f'{folder}/c.txt', c)
+
+    A = program["A"]
+    row_ixs, col_ixs = A.nonzero()
+    with open(f"{folder}/A.txt", "w") as file:
+        file.write("\t".join(map(str, row_ixs)))
+        file.write("\n")
+        file.write("\t".join(map(str, col_ixs)))
+        file.write("\n")
+        file.write("\t".join(map(str, A.data)))
+        file.write("\n")
+    np.savetxt(f'{folder}/b.txt', program['b'])
+    np.savetxt(f'{folder}/c.txt', program['c'])
+    if "x_star" in program:
+        np.savetxt(f'{folder}/x_star.txt', program["x_star"])
+        np.savetxt(f'{folder}/y_star.txt', program["y_star"])
+        np.savetxt(f'{folder}/s_star.txt', program["s_star"])
 
 
 def load_cone_program(folder):
-    A = sparse.load_npz(f'{folder}/A.npz')
+    with open(f"{folder}/A.txt", "r") as file:
+        row_ixs, col_ixs, vals = file.readlines()
+        # TODO: finish formatting into sparse matrix
     b = np.loadtxt(f'{folder}/b.txt')
     c = np.loadtxt(f'{folder}/c.txt')
-    return A, b, c
+    if os.path.exists(f'{folder}/x_star.txt'):
+        x_star = np.loadtxt(f'{folder}/x_star.txt')
+        y_star = np.loadtxt(f'{folder}/y_star.txt')
+        s_star = np.loadtxt(f'{folder}/s_star.txt')
+        return dict(A=A, b=b, c=c, x_star=x_star, y_star=y_star, s_star=s_star)
+    else:
+        return dict(A=A, b=b, c=c)
 
 
 if __name__ == '__main__':
@@ -41,5 +62,6 @@ if __name__ == '__main__':
     m = 3 + 3 + 5
     n = 5
 
-    A, b, c = random_cone_prog(m, n, cone_dict)
-    save_cone_program("random_programs/test/", A, b, c)
+    program = random_cone_prog(m, n, cone_dict)
+    save_cone_program("random_programs/test", program)
+    p = load_cone_program("random_programs/test")
