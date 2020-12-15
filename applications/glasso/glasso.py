408c3494db98d75872218cffb1c10702731d1644
Chandler Squires <chandlersquires18@gmail.com>
2020-12-15 14:17:24 -0500

    switch Z to lower triangular

--- applications/glasso/glasso.py
+++ applications/glasso/glasso.py
@@ -19,8 +19,8 @@ sol = X.value
 
 
 def _symmetric2vec(A):
-    B = A + A.T
-    B[np.diag_indices_from(B)] /= 2
+    B = A * sqrt2
+    B[np.diag_indices_from(B)] /= sqrt2
     return B.T[np.triu_indices_from(B)]
 
 
@@ -46,20 +46,20 @@ def merge_psd_plus_diag(theta_size, z_size, p):
         i += p
 
     # === Z ===
-    i, j = p, theta_size
-    for k in range(p, 0, -1):
-        for k_ in range(k):
-            A[i, j] = sqrt2
-            i += 1
+    i, j = p, theta_size+p
+    for d in range(p):
+        zd_ix = theta_size + d
+        A[i, zd_ix] = sqrt2
+        for d_ in range(d):
+            A[i-d_-1, j] = sqrt2
             j += 1
-        i += p
+        i += (2*p - d)
 
     # === DIAGONAL ===
-    j = theta_size  # start at first z1
     i = int(p**2 + p*(p+1)/2)
     for d in range(p):
-        A[i, j] = 1
-        j += p-d
+        z_ix = theta_size + d
+        A[i, z_ix] = 1
         i += (p-d)
     return A
 
@@ -78,14 +78,13 @@ def write_glasso_cone_program(S, lambda_):
 
     # log-det inequality constraint on t_i
     A2 = np.zeros((3*p, z_size+p))
-    z_ix = 0
     b2 = np.zeros(3*p)
     for d in range(p):
+        z_ix = d
         t_ix = z_size+d
         A2[d*3, z_ix] = -1
         b2[d*3+1] = 1
         A2[d*3+2, t_ix] = -1
-        z_ix += p-d
     print(A2)
 
     # Equality constraint on t
@@ -135,9 +134,11 @@ if __name__ == '__main__':
     a = np.random.normal(size=(100, 3))
     S = np.corrcoef(a, rowvar=False)
     A, b, c, cone_dict = write_glasso_cone_program(S, 1.)
+    x = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16])
+    print(_vec2symmetric((A @ x)[1:22], 6))
     x = Matrix(sympy.symbols([
         "theta11", "theta21", "theta31", "theta22", "theta32", "theta33",
-        "z11", "z21", "z31", "z22", "z32", "z33",
+        "z11", "z22", "z33", "z21", "z31", "z32",
         "t1", "t2", "t3", "t"
     ]))
 
@@ -145,7 +146,7 @@ if __name__ == '__main__':
     print(Matrix(c).T @ x)
     # sol = diffcp.solve_and_derivative(A, b, c, cone_dict)
     K = np.linalg.inv(S)
-    sol = scs.solve(dict(A=A, b=b, c=c), cone_dict, eps=1e-12, max_iters=2000, verbose=True, acceleration_lookback=10)
+    sol = scs.solve(dict(A=A, b=b, c=c), cone_dict, eps=1e-12, max_iters=2000, verbose=True, acceleration_lookback=1)
     x = sol["x"]
     theta = _vec2symmetric(x[:6], 3)
 
