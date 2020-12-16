48a56994fbebf4ad3d762a05e7477a7ba9931191
Theo Diamandis <tjdiamandis@gmail.com>
2020-12-16 14:39:13 -0500

    add power cone tests

--- scratch/julia_testing/test_proj.jl
+++ scratch/julia_testing/test_proj.jl
@@ -87,13 +87,16 @@ function _test_d_proj(cone, tol)
     dproj_finite_diff = proj_xdx - proj_x
 
     dproj_test = ConeProgramDiff._d_proj(x, cone) * dx
-    @assert isapprox(dproj_finite_diff, dproj_test, atol=tol)
+    if !isapprox(dproj_finite_diff, dproj_test, atol=tol)
+        println("Error!")
+        error("Error for x = $x, dx = $dx, $cone")
+    end
 end
 
 
 function test_d_proj(cone::MOI.AbstractVectorSet; tol=1e-8)
     Random.seed!(0)
-    for _ in 1:10
+    for _ in 1:100
         _test_d_proj(cone, tol)
         _test_d_proj(MOI.dual_set(cone), tol)
     end
@@ -101,20 +104,21 @@ end
 
 
 function test_d_proj(n::Int; tol=tol)
-    d_proj_cones = [
+    cones = [
         MOI.Zeros(n),
         MOI.Nonnegatives(n),
         MOI.SecondOrderCone(n),
         MOI.PositiveSemidefiniteConeTriangle(n)
     ]
     for cone in cones
+        println("Testing $cone")
         test_d_proj(cone; tol=tol)
     end
 end
 
 
 function test_d_proj_exp(tol)
-    function _helper(x, tol; dual)
+    function _helper(x, dx, tol; dual)
         proj_x = ConeProgramDiff._proj_exp_cone(x,  dual=dual)
         proj_xdx = ConeProgramDiff._proj_exp_cone(x+dx,  dual=dual)
         dproj_finite_diff = proj_xdx - proj_x
@@ -131,16 +135,17 @@ function test_d_proj_exp(tol)
         x = randn(3)
         dx = 1e-6 * randn(3)
         case_p[det_case_exp_cone(x; dual=false)] += 1
-        println(det_case_exp_cone(x; dual=false))
-        _helper(x, tol; dual=false)
+        # println(det_case_exp_cone(x; dual=false))
+        _helper(x, dx, tol; dual=false)
 
         case_d[det_case_exp_cone(x; dual=true)] += 1
         # println(det_case_exp_cone(x; dual=true))
         # println(x)
-        _helper(x, tol; dual=true)
+        _helper(x, dx, tol; dual=true)
     end
     @assert all(case_p .> 0) && all(case_d .> 0)
 end
+
 
 
 ## Testing Projections
@@ -148,8 +153,8 @@ test_proj_zero()
 test_proj_pos()
 test_proj_soc()
 test_proj_psd()
-test_d_proj(10)
-test_d_proj_exp(1e-6)
+test_d_proj(10; tol=1e-6)
+test_d_proj_exp(1e-5)
 
 
 ## Test pi and project_onto_cone functions
@@ -224,8 +229,8 @@ tol
 test_project_onto_cone()
 test_d_project_onto_cone()
 
-## Test Exponential Cone
 
+## Test Exponential Cone
 function det_case_exp_cone(v; dual=false)
     v = dual ? -v : v
     if ConeProgramDiff.in_exp_cone(v)
@@ -273,13 +278,114 @@ end
 
 test_proj_exp(1e-7)
 
-x = rand(3)
-ConeProgramDiff._d_proj_exp_cone(x, dual=false)
 
-# sum(abs.(v - (vp + vd)))
-# dot(vp, vd)
-# in_exp_cone(vp)
-# in_exp_cone_dual(vd)
-# x
-# get_exp_proj_case4(x)
-# z_star
+function det_case_pow_cone(v, α; dual=false)
+    v = dual ? -v : v
+    x, y, z = v
+
+    if ConeProgramDiff.in_pow_cone(x, y, z, α)
+        # if in power cone
+        return 1
+    elseif ConeProgramDiff.in_pow_cone_polar(x, y, z, α)
+        # if in polar cone Ko = -K*
+        return 2
+    elseif abs(z) <= 1e-6
+        # if not in K, Ko and z = 0
+        return 3
+    else
+        return 4
+    end
+end
+
+function test_proj_pow_cone(tol)
+    function _test_proj_pow_cone_help(x, α, tol; dual=false)
+        cone = dual ? MOI.DualPowerCone(α) : MOI.PowerCone(α)
+        model = Model()
+        set_optimizer(model, optimizer_with_attributes(
+            SCS.Optimizer, "eps" => 1e-8, "max_iters" => 10000,
+            "verbose" => 0, "scale" => 10.0))
+        @variable(model, z[1:3])
+        @variable(model, t)
+        @objective(model, Min, t)
+        @constraint(model, sum((x-z).^2) <= t)
+        @constraint(model, z in cone)
+        optimize!(model)
+        z_star = value.(z)
+        xp = ConeProgramDiff._proj_pow_cone(x, α, dual=dual)
+        if !isapprox(xp, z_star, atol=tol)
+            error("x = $x,\nz* = $z_star,\nxp = $xp,\ncase = $(det_case_pow_cone(x, α; dual=dual))")
+        end
+    end
+    Random.seed!(0)
+    n = 3
+    case_p = zeros(4)
+    case_d = zeros(4)
+    for _ in 1:100
+        x = randn(3)
+        α = rand()*0.95 + 0.025
+        # α = rand()
+        # println("$x, $α")
+
+        # Need to get some into case 3
+        if rand(1:10) == 1
+            x[3] = 0
+        end
+        case_p[det_case_pow_cone(x, α; dual=false)] += 1
+        _test_proj_pow_cone_help(x, α, tol; dual=false)
+
+        case_d[det_case_pow_cone(x, α; dual=true)] += 1
+        _test_proj_pow_cone_help(x, α, tol; dual=true)
+    end
+
+    @assert all(case_p .> 0) && all(case_d .> 0)
+end
+
+
+function test_d_proj_pow(tol)
+    function _helper(x, dx, tol; dual)
+        # println("$x \t $dx \t $(x+dx)")
+        proj_x = ConeProgramDiff._proj_pow_cone(x, α; dual=dual)
+        proj_xdx = ConeProgramDiff._proj_pow_cone(x+dx, α; dual=dual)
+        dproj_finite_diff = proj_xdx - proj_x
+        dproj_test = ConeProgramDiff._d_proj_pow_cone(x, α; dual=dual) * dx
+        # println(dproj_finite_diff)
+        # println(dproj_test)
+        @assert isapprox(dproj_finite_diff, dproj_test, atol=tol)
+    end
+
+    Random.seed!(0)
+    case_p = zeros(4)
+    case_d = zeros(4)
+    for _ in 1:100
+        x = randn(3)
+        dx = 1e-6 * randn(3)
+        α = rand()*0.8 + 0.1
+
+        # Need to get some into case 3
+        if rand(1:10) == 1
+            x[3] = 0
+        end
+
+        case_p[det_case_pow_cone(x, α; dual=false)] += 1
+        _helper(x, dx, tol; dual=false)
+
+        case_d[det_case_pow_cone(x, α; dual=true)] += 1
+        _helper(x, dx, tol; dual=true)
+    end
+    @assert all(case_p .> 0) && all(case_d .> 0)
+end
+
+
+
+x,y,z = -[0.7106827995942762; -0.3457209671448468; -0.8353979677680569]
+α = 0.2748063801261257
+Phi(x,y,z,α,r) = 0.5*(Phi_prod(x,α,z,r)^α * Phi_prod(y,1-α,z,r)^(1-α)) - r
+Phi_prod(xi,αi,z,r) = (xi + sqrt(xi^2 + 4*αi*r*(abs(z) - r)))
+POW_CONE_THRESH = 1e-14
+lb_ub = (0.0+POW_CONE_THRESH, abs(z)-POW_CONE_THRESH)
+h(r) = Phi(x,y,z,α,r)
+h(lb_ub[1])
+h(lb_ub[2])
+r = find_zero(r -> Phi(x,y,z,α,r), lb_ub, verbose=false)
+
+test_d_proj_pow(1e-6)
