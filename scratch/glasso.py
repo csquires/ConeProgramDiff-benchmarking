5e2a31b823da9d661774949d12a073f8c24f7641
Chandler Squires <csquires@mit.edu>
2020-12-15 20:14:16 -0500

    check that adding variable works same as absolute value

--- scratch/glasso.py
+++ scratch/glasso.py
@@ -3,87 +3,113 @@ import numpy as np
 import diffcp
 from scipy.sparse import csc_matrix
 import numpy as np
+import itertools as itr
 
 sqrt2 = np.sqrt(2)
 sqrt2 = 1
+# sqrt2 = 1/np.sqrt(2)
 
 p = 3
-lambda_ = 0
+lambda_ = 1
 A = np.random.normal(size=(p*2, p))
 S = A.T @ A
+print("true inverse")
+print(np.linalg.inv(S))
+
+print("original form, no l1 penalty")
 X = cp.Variable((p, p), symmetric=True)
 constraints = [X >> 0]
 objective = cp.Minimize(cp.sum(cp.multiply(S, X)) - cp.log_det(X))
 prob = cp.Problem(objective, constraints)
 prob.solve()
 sol = X.value
-print("true inverse")
-print(np.linalg.inv(S))
-print("original form")
 print(sol)
 
-print("form with t added")
+print("original form, with l1 penalty")
 X = cp.Variable((p, p), symmetric=True)
-t = cp.Variable()
-constraints = [X >> 0, t <= cp.log_det(X)]
-objective = cp.Minimize(cp.sum(cp.multiply(S, X)) - t)
+constraints = [X >> 0]
+objective = cp.Minimize(cp.sum(cp.multiply(S, X)) - cp.log_det(X) + lambda_*cp.pnorm(X, 1))
 prob = cp.Problem(objective, constraints)
 prob.solve()
 sol = X.value
 print(sol)
 
-print("form with log det expanded")
+print("form with l1 penalty expressed via extra variables")
 X = cp.Variable((p, p), symmetric=True)
-Z = cp.Variable((p, p))
-D = cp.diag(cp.diag(Z))
-A = cp.vstack([
-    cp.hstack([X, Z]),
-    cp.hstack([cp.transpose(Z), D])]
-)
-t = cp.Variable()
+M = cp.Variable((p, p), symmetric=True)
 constraints = [
-    A >> 0,
-    t <= cp.sum(cp.log(cp.diag(Z))),
-    Z[0, 1] == 0,
-    Z[0, 2] == 0,
-    Z[1, 2] == 0,
+    X >> 0,
+    *[M[i, j] >= X[i, j] for i, j in itr.product(range(p), range(p))],
+    *[M[i, j] >= -X[i, j] for i, j in itr.product(range(p), range(p))],
 ]
-objective = cp.Minimize(cp.sum(cp.multiply(S, X)) - t)
+objective = cp.Minimize(cp.sum(cp.multiply(S, X)) - cp.log_det(X) + lambda_*cp.sum(M))
 prob = cp.Problem(objective, constraints)
 prob.solve()
 sol = X.value
 print(sol)
 
-print("form with t as a sum")
-X = cp.Variable((p, p), symmetric=True)
-Z = cp.Variable((p, p))
-D = cp.diag(cp.diag(Z))
-A = cp.vstack([
-    cp.hstack([X, Z]),
-    cp.hstack([cp.transpose(Z), D])]
-)
-t = cp.Variable()
-t1 = cp.Variable()
-t2 = cp.Variable()
-t3 = cp.Variable()
-constraints = [
-    A >> 0,
-    Z[0, 1] == 0,
-    Z[0, 2] == 0,
-    Z[1, 2] == 0,
-    # t1 <= cp.log(Z[0, 0]),
-    # t2 <= cp.log(Z[1, 1]),
-    # t3 <= cp.log(Z[2, 2]),
-    cp.constraints.exponential.ExpCone(t1, 1, Z[0, 0]),
-    cp.constraints.exponential.ExpCone(t2, 1, Z[1, 1]),
-    cp.constraints.exponential.ExpCone(t3, 1, Z[2, 2]),
-    t == t1 + t2 + t3,
-]
-objective = cp.Minimize(cp.sum(cp.multiply(S, X)) - t)
-prob = cp.Problem(objective, constraints)
-prob.solve()
-sol = X.value
-print(sol)
+# print("form with t added")
+# X = cp.Variable((p, p), symmetric=True)
+# t = cp.Variable()
+# constraints = [X >> 0, t <= cp.log_det(X)]
+# objective = cp.Minimize(cp.sum(cp.multiply(S, X)) - t)
+# prob = cp.Problem(objective, constraints)
+# prob.solve()
+# sol = X.value
+# print(sol)
+
+# print("form with log det expanded")
+# X = cp.Variable((p, p), symmetric=True)
+# Z = cp.Variable((p, p))
+# D = cp.diag(cp.diag(Z))
+# A = cp.vstack([
+#     cp.hstack([X, Z]),
+#     cp.hstack([cp.transpose(Z), D])]
+# )
+# t = cp.Variable()
+# constraints = [
+#     A >> 0,
+#     t <= cp.sum(cp.log(cp.diag(Z))),
+#     Z[0, 1] == 0,
+#     Z[0, 2] == 0,
+#     Z[1, 2] == 0,
+# ]
+# objective = cp.Minimize(cp.sum(cp.multiply(S, X)) - t)
+# prob = cp.Problem(objective, constraints)
+# prob.solve()
+# sol = X.value
+# print(sol)
+#
+# print("form with t as a sum")
+# X = cp.Variable((p, p), symmetric=True)
+# Z = cp.Variable((p, p))
+# D = cp.diag(cp.diag(Z))
+# A = cp.vstack([
+#     cp.hstack([X, Z]),
+#     cp.hstack([cp.transpose(Z), D])]
+# )
+# t = cp.Variable()
+# t1 = cp.Variable()
+# t2 = cp.Variable()
+# t3 = cp.Variable()
+# constraints = [
+#     A >> 0,
+#     Z[0, 1] == 0,
+#     Z[0, 2] == 0,
+#     Z[1, 2] == 0,
+#     # t1 <= cp.log(Z[0, 0]),
+#     # t2 <= cp.log(Z[1, 1]),
+#     # t3 <= cp.log(Z[2, 2]),
+#     cp.constraints.exponential.ExpCone(t1, 1, Z[0, 0]),
+#     cp.constraints.exponential.ExpCone(t2, 1, Z[1, 1]),
+#     cp.constraints.exponential.ExpCone(t3, 1, Z[2, 2]),
+#     t == t1 + t2 + t3,
+# ]
+# objective = cp.Minimize(cp.sum(cp.multiply(S, X)) - t)
+# prob = cp.Problem(objective, constraints)
+# prob.solve()
+# sol = X.value
+# print(sol)
 
 
 def _symmetric2vec(A):
@@ -194,34 +220,34 @@ def write_glasso_cone_program(S, lambda_):
     return A_, b_, c, cone_dict
 
 
-if __name__ == '__main__':
-    import sympy
-    from sympy import Matrix
-    import scs
-    import ecos
-    a = np.random.normal(size=(100, 6))
-    S = np.cov(a, rowvar=False)
-    A, b, c, cone_dict = write_glasso_cone_program(S, 1.)
-    # x = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16])
-    # print(_vec2symmetric((A @ x)[1:22], 6))
-    # x = Matrix(sympy.symbols([
-    #     "theta11", "theta21", "theta31", "theta22", "theta32", "theta33",
-    #     "z11", "z22", "z33", "z21", "z31", "z32",
-    #     "t1", "t2", "t3", "t"
-    # ]))
-    #
-    # print(Matrix(b) - Matrix(A.toarray()) @ x)
-    # print(Matrix(c).T @ x)
-    # sol = diffcp.solve_and_derivative(A, b, c, cone_dict)
-    K = np.linalg.inv(S)
-    sol = scs.solve(dict(A=A, b=b, c=c), cone_dict, eps=1e-15, max_iters=10000, verbose=True, acceleration_lookback=1)
-    x = sol["x"]
-    p = S.shape[0]
-    d = int(S.shape[0]*(S.shape[0]+1)/2)
-    theta = _vec2symmetric(x[:d], p)
-    print(theta[:3, :3])
-    print(np.linalg.inv(S)[:3, :3])
-    print(np.linalg.norm(theta - np.linalg.inv(S), "fro"))
+# if __name__ == '__main__':
+#     import sympy
+#     from sympy import Matrix
+#     import scs
+#     import ecos
+#     a = np.random.normal(size=(100, 3))
+#     S = np.cov(a, rowvar=False)
+#     A, b, c, cone_dict = write_glasso_cone_program(S, 1.)
+#     x = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16])
+#     print(_vec2symmetric((A @ x)[1:22], 6))
+#     x = Matrix(sympy.symbols([
+#         "theta11", "theta21", "theta31", "theta22", "theta32", "theta33",
+#         "z11", "z22", "z33", "z21", "z31", "z32",
+#         "t1", "t2", "t3", "t"
+#     ]))
+#
+#     print(Matrix(b) - Matrix(A.toarray()) @ x)
+#     print(Matrix(c).T @ x)
+#     sol = diffcp.solve_and_derivative(A, b, c, cone_dict)
+#     K = np.linalg.inv(S)
+#     sol = scs.solve(dict(A=A, b=b, c=c), cone_dict, eps=1e-15, max_iters=10000, verbose=True, acceleration_lookback=1)
+#     x = sol["x"]
+#     p = S.shape[0]
+#     d = int(S.shape[0]*(S.shape[0]+1)/2)
+#     theta = _vec2symmetric(x[:d], p)
+#     print(theta[:3, :3])
+#     print(np.linalg.inv(S)[:3, :3])
+#     print(np.linalg.norm(theta - np.linalg.inv(S), "fro"))
 
     # theta = [1, 2, 3, 4, 5, 6]
     # z = [7, 8, 9, 10, 11, 12]
