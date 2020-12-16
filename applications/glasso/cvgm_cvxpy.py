0c1035fb61a0ac378da38c96422c3b9fcf51a845
Chandler Squires <csquires@mit.edu>
2020-12-15 22:31:13 -0500

    try evaluating graphical lasso

--- applications/glasso/cvgm_cvxpy.py
+++ applications/glasso/cvgm_cvxpy.py
@@ -11,7 +11,7 @@ def _vec2symmetric(a, dim):
     return A
 
 
-def gradient_descent_update(p, p_grad, eta=.01):
+def gradient_descent_update(p, p_grad, eta=.1):
     return p - eta * p_grad
 
 
@@ -34,6 +34,7 @@ def cvgm_glasso(samples, alpha0, K=10, p=0.7, tol=1e-4, max_iters=20, gradient_u
         num_iter += 1
         gradients = []
 
+        print(f"=== Iteration {num_iter}. alpha={curr_alpha} ====")
         for training_data, test_data in zip(training_datasets, test_datasets):
             cov_train = np.cov(training_data, rowvar=False)
             p = cov_train.shape[1]
@@ -46,23 +47,26 @@ def cvgm_glasso(samples, alpha0, K=10, p=0.7, tol=1e-4, max_iters=20, gradient_u
             prob = cp.Problem(objective, constraints)
             prob.solve(requires_grad=True)
             theta = X.value
-            # write program in correct form and run it
 
-            print("=====")
-            print(f"Alpha: {curr_alpha}")
-            print(f"Training loss: {np.sum(cov_train * theta) - np.log(np.linalg.det(theta))}")
+            print(f"Training loss: {.5*np.sum(cov_train * theta) - .5*np.log(np.linalg.det(theta)) + p/2 * np.log(2*np.pi)}")
 
             cov_test = np.cov(test_data, rowvar=False)
-            print(f"Test loss: {np.sum(cov_test * theta) - np.log(np.linalg.det(theta))}")
+            # print(f"Test loss: {np.sum(cov_test * theta) - np.log(np.linalg.det(theta))}")
+            print(f"Test loss: {.5*np.sum(cov_test * theta) - .5*np.log(np.linalg.det(theta)) + p/2 * np.log(2*np.pi)}")
             dl_dtheta = cov_test - np.linalg.inv(theta)
 
             alpha_cp.delta = 1
             prob.derivative()
             dtheta_d_alpha = X.delta
 
+            X.gradient = cov_test - np.linalg.inv(theta)
+            prob.backward()
+            dl_dalpha = alpha_cp.gradient
+            # print("Method 1:", dl_dalpha)
             dl_dalpha = np.sum(dl_dtheta * dtheta_d_alpha)
-            print(dl_dalpha)
+            # print("Method 2:", dl_dalpha)
             gradients.append(dl_dalpha)
+
         print(f"Gradient Average: {np.mean(gradients)}")
         print(f"Gradient Std: {np.std(gradients)}")
 
@@ -72,25 +76,46 @@ def cvgm_glasso(samples, alpha0, K=10, p=0.7, tol=1e-4, max_iters=20, gradient_u
         curr_alpha = gradient_update(curr_alpha, alpha_grad)
         curr_alpha = max(curr_alpha, 0)
 
-        print(alpha_grad, curr_alpha)
-
-    # s = prob(samples, new_alpha)
-    return curr_alpha
+    # solve again
+    X = cp.Variable((p, p), symmetric=True)
+    constraints = [X >> 0]
+    objective = cp.Minimize(cp.sum(cp.multiply(cov_train, X)) - curr_alpha*cp.log_det(X))
+    prob = cp.Problem(objective, constraints)
+    prob.solve()
+    theta = X.value
+    return curr_alpha, theta
 
 
 if __name__ == '__main__':
     import causaldag as cd
     from sklearn.covariance import GraphicalLassoCV
 
-    d = cd.rand.directed_erdos(5, .5)
+    d = cd.rand.directed_erdos(10, .2)
     g = cd.rand.rand_weights(d)
-    samples = g.sample(100)
+    samples = g.sample(1000)
 
-    cvgm_alpha = cvgm_glasso(samples, 1)
+    cvgm_alpha, cvgm_theta = cvgm_glasso(samples, alpha0=.4, K=20)
     print(cvgm_alpha)
 
-    glcv = GraphicalLassoCV(alphas=[.1, .2, .3, .4, .5, .6, .7, .8, .9], cv=10)
+    glcv = GraphicalLassoCV(alphas=[.5, 1, 2], cv=10)
     glcv.fit(samples)
-    print(glcv.alpha_)
 
+    print(f"Selected alpha from standard CV: {glcv.alpha_}")
+    print(f"Alphas: {glcv.cv_alphas_}")
+    print(f"Average log likelihoods: {glcv.grid_scores_.mean(axis=1)}")
 
+    heldout_samples = g.sample(1000)
+    cov_heldout = np.cov(heldout_samples, rowvar=False)
+    p = cov_heldout.shape[0]
+    glcv_theta = np.linalg.inv(glcv.covariance_)
+    print(f"CVGM loss: {.5*np.sum(cov_heldout * cvgm_theta) - .5*np.log(np.linalg.det(cvgm_theta)) + p/2 * np.log(2*np.pi)}")
+    print(f"CV loss: {.5*np.sum(cov_heldout * glcv_theta) - .5*np.log(np.linalg.det(glcv_theta)) + p/2 * np.log(2*np.pi)}")
+
+    glcv_edges = set(zip(*np.nonzero(~np.isclose(glcv_theta, 0))))
+    cvgm_edges = set(zip(*np.nonzero(~np.isclose(cvgm_theta, 0))))
+    print(len(d.moral_graph().edges - glcv_edges))
+    print(len(d.moral_graph().edges - cvgm_edges))
+    print(len(glcv_edges - d.moral_graph().edges))
+    print(len(cvgm_edges - d.moral_graph().edges))
+    print(np.nonzero(~np.isclose(glcv_theta, 0)))
+    print(np.nonzero(~np.isclose(cvgm_theta, 0)))
